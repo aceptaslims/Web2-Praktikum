1d1541fe3792543378a9c8100b6639246d26c67f
aceptaslims <teknikomputer01@gmail.com>
2026-04-20 21:05:34 +0700

    menambahkan folder

---
+++ Tugas_model/app/Models/BookReturn.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class BookReturn extends Model
+{
+    protected $table = 'returns';
+    public $timestamps = false; 
+
+    protected $fillable = [
+        'loan_detail_id',
+        'charge',
+        'amount',
+    ];
+
+    public function loanDetail(): BelongsTo
+    {
+        return $this->belongsTo(LoanDetail::class, 'loan_detail_id');
+    }
+}
